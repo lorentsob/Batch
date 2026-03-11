c22d882e5504f5b5dc60c71914996ab62210c95b
lorentso <lorenzoboschi39@gmail.com>
2026-03-11 09:32:24 +0100

    fase 9

--- fix_bakes_view.py
+++
@@ -1,7 +0,0 @@
-with open("Levain/Features/Bakes/BakesView.swift", "r") as f:
-    lines = f.readlines()
-
-new_lines = lines[:163] + lines[507:]
-
-with open("Levain/Features/Bakes/BakesView.swift", "w") as f:
-    f.writelines(new_lines)
